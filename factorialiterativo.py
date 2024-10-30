bb6bc6f5abb218f4c1f5e1c44ef4e0ef9b881884
Raul5304 <raullecinena@iesrioarba.es>
2024-10-30 11:06:14 +0100

    first commit

---
+++ factorialiterativo.py
@@ -0,0 +1,22 @@
+#! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+def factorial(n):
+    """factorial iterativo descendente"""
+    fact = 1
+    actual = n
+    while actual > 0:
+        fact = fact * actual
+        actual = actual - 1
+    return fact
+    
+    
+if __name__ == "__main__":
+    
+    x = 3
+    print(factorial(x)) # 6
+    
+    x = 2
+    print(factorial(x)) # 2
+    
+    x = 1
+    print(factorial(x)) # 1
---
+++ main.py
@@ -0,0 +1,10 @@
+#! /C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
+
+from factorialrecursivo import factorial
+# from factorialiterativo import factorial 
+    
+if __name__ == "__main__":
+    
+    for i in range(30):
+        print(i, factorial(i))
+    

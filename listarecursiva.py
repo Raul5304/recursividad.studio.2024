8f2fdcd87f28f216434b0c80bf80451a1c17afaa
Raul5304 <raullecinena@iesrioarba.es>
2024-10-31 12:25:06 +0100

    .

--- listarecursiva.py
+++ listarecursiva.py
@@ -3,14 +3,14 @@
 def imprime(lista):
     """impresión recursiva de una lista
     con enfoque primero ; restantes"""
-    print("<", end="")
+    # casos triviales ... 
+    print("( ", end="")
     if len(lista) == 0:
-        print(">", end="")
+        print(")", end="")
         return
-    primero = lista[0]
-    listaRestantes = lista[1:]
-    print(primero, end=", ")
-    imprime(listaRestantes)
+    print(lista[0], end=", ") # ... primero
+    imprime(lista[1:]) # restantes: Recursion
+    print(")", end="")
     
     
     
@@ -18,12 +18,16 @@ if __name__ == "__main__":
     
     l = [33,22,11]
     imprime(l)
+    print()
     
-    # l = [33,22]
-    # imprime(l)
+    l = [22,11]
+    imprime(l)
+    print()
+
+    l = [11]
+    imprime(l)
+    print()
     
-    # l = [33]
-    # imprime(l)
-    
-    # l = []
-    # imprime(l)
+    l = []
+    imprime(l)
+    print()
